9f7e138dabd207d33bfaae8defd1d757dcca82f3
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-03-06 15:47:41 -0300

    3- Exibe um alert informando que a tarefa foi adicionada com sucesso

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ let tarefas = [
 document.querySelector('.botao__adicionar').addEventListener('click', () => {
     // Captura o valor digitado no campo de input (título da tarefa) e remove espaços extras no começo e no final
     const titulo = document.querySelector('.texto__tarefa').value.trim();
-})
+
+    // Verifica se o título não está vazio
+    if (titulo) {
+        // Questão 3 - Exibir um alert informando que a tarefa foi adicionada com sucesso
+        alert('Tarefa adicionada com sucesso!');
+        
+        // Adiciona a nova tarefa ao array 'tarefas'. Usa o operador spread ( ... ) para copiar as tarefas existentes e adicionar a nova tarefa com um id único
+        tarefas = [...tarefas, { id: tarefas.length + 1, titulo, concluida: false }];
+}})
