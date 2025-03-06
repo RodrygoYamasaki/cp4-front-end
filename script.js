efb16dc1dc59d433e0ccd12382cd2f5a4dedba31
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-03-06 15:45:52 -0300

    1- Criando um array para armazenar as tarefas

---
+++ script.js
@@ -0,0 +1,4 @@
+// Questão 1 - Criar um array para armazenar as tarefas
+let tarefas = [
+    { id: 1, titulo: 'Tarefa Inicial', concluida: false } // A primeira tarefa do array, com id 1, título 'Tarefa Inicial' e status de não concluída
+];
