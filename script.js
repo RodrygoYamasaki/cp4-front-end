63cc0356d610c0fab68b2493eddcc8c83b69b0bd
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-03-06 15:46:48 -0300

    2- Adicionando um evento ao botão para capturar o valor do input e criar uma nova tarefa

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 let tarefas = [
     { id: 1, titulo: 'Tarefa Inicial', concluida: false } // A primeira tarefa do array, com id 1, título 'Tarefa Inicial' e status de não concluída
 ];
+
+// Questão 2 - Adicionar um evento ao botão para capturar o valor do input e criar uma nova tarefa
+document.querySelector('.botao__adicionar').addEventListener('click', () => {
+    // Captura o valor digitado no campo de input (título da tarefa) e remove espaços extras no começo e no final
+    const titulo = document.querySelector('.texto__tarefa').value.trim();
+})
